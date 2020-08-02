edb53acbce2ce652803f413f57589c92a2488dc0
Helen Zhao <hzhao4@uw.edu>
2020-08-01 23:02:08 -0700

    Finished half of problem b

--- problem-b/js/index.js
+++ problem-b/js/index.js
@@ -1,70 +1,109 @@
 'use strict';
 
-/* Define a function `addFour()` that takes a single argument 
+/* Define a function `addFour()` that takes a single argument
    and returns a value 4 greater than the input.*/
+function addFour(number) {
+  return number + 4;
+}
 
-   
 /* Create and log a variable `twelve` that is the result of passing 8 to your
    addFour() function. */
+let twelve = addFour(8);
+console.log(twelve);
 
-   
-/* Create and log a variable `twelveString` that is the result of passing "8" 
+/* Create and log a variable `twelveString` that is the result of passing "8"
    (a string) to your addFour() function. Consider what this tells you about how
   the function should be explained (e.g., in a comment). */
+// numbers perform addition; strings behave like that of Java strings
 
-  
+let twelveString = addFour('8');
+  console.log(twelveString);
 
-/* Define a function `compoundInterest()` that takes three parameters: 
+/* Define a function `compoundInterest()` that takes three parameters:
      1. an initial bank balance (principle, in dollars)
-     2. an annual interest rate (as a decimal, e.g., 0.01) 
+     2. an annual interest rate (as a decimal, e.g., 0.01)
      3. a number of years
    The function should calculate the continuous compound interest
-     (https://en.wikipedia.org/wiki/Compound_interest#Continuous_compounding) 
+     (https://en.wikipedia.org/wiki/Compound_interest#Continuous_compounding)
    and *return* the resulting total balance after that many number of years
 
    You can call the method and log the result to check your work. Compare to
      http://www.mathwarehouse.com/calculators/continuous-compound-interest-calculator.php
 */
-
-
+function compoundInterest(principle, interestRate, years) {
+  let result = principle * Math.pow(Math.E, interestRate * years);
+  return result;
+}
 
 /* Define a function `fizzBuzz()` that takes in a single number as an argument.
-   The function should *return* an _array_ of numbers from 1 to the argument. 
-   But for multiples of three, the array should contain "Fizz" instead of the 
-   number. For multiples of five, the array should contain "Buzz" instead of the 
-   number. For numbers which are multiples of both three and five, the array 
+   The function should *return* an _array_ of numbers from 1 to the argument.
+   But for multiples of three, the array should contain "Fizz" instead of the
+   number. For multiples of five, the array should contain "Buzz" instead of the
+   number. For numbers which are multiples of both three and five, the array
    should contain "FizzBuzz" instead of the number.
    The returned array should be empty for arguments less than 1. */
+function fizzBuzz(number) {
+  if (number <  1) {
+    return [];
+  }
+  let array = [];
+  for (let i = 0; i < number; i++) {
+    if (number % 3 == 0) {
+      array.push("Fizz");
+    } else if (number % 5 == 0) {
+      array.push("Buzz");
+    } else if (number % 5 && number % 3 == 0) {
+      array.push("FizzBuzz");
+    } else {
+      array.push(number);
+    }
+  }
+  return array;
+}
 
-   
 
-/* Define a function `getLetterFrequencies()` that takes in a single string as 
+/* Define a function `getLetterFrequencies()` that takes in a single string as
    an argument. The function should *return* an Object whose keys are characters
    (letters) and whose values are the number of times that character appears in
    the argument. Your function should be case sensitive.
    _Hint:_ start with an empty Object, then loop through the letters one at a
-   time (you can access them with bracket notation, like with an array). For 
-   each letter, increase the value associated with that key by one. Watch out 
+   time (you can access them with bracket notation, like with an array). For
+   each letter, increase the value associated with that key by one. Watch out
    for if the letter is not in the Object yet!
    You can test this method with a word like "Mississippi". */
-
-   
+function getLetterFrequencies(word) {
+  let result = {};
+  for (let i = 0; i < word.length; i++) {
+    if (result.contains(word.charAt(i))) {
+      result[word.charAt(i)]++;
+    } else {
+      result[word.charAt(i)] = 1;
+    }
+  }
+}
 
 /* Create a variable `deck` that represents a deck of modern playing cards
    (https://en.wikipedia.org/wiki/Playing_card). This variable should be an
    *array* of 52 elements, each of which is an Object with properties:
-     - `suit`, with a string value that is either `"hearts"`, `"diamonds"`, 
+     - `suit`, with a string value that is either `"hearts"`, `"diamonds"`,
        `"clubs"`, or `"spades"`
-     - `rank`, with an integer value ranging from 2 to 14 inclusive (values 
+     - `rank`, with an integer value ranging from 2 to 14 inclusive (values
         11-14 represent a Jack, Queen, King, or Ace respectively).
-    Tip: use a pair of nested loops to add each combination of suit and rank to 
-    the `deck` array! 
-    
+    Tip: use a pair of nested loops to add each combination of suit and rank to
+    the `deck` array!
+
     You can log out the `deck` to check your work! */
+    var deck = [];
+    let count = 0;
+    let suits = ["hearts", "diamonds", "clubs", "spades"];
+    for (let i = 0; i < 13; i++) {
+      for (let j = 0; j < 4; j++) {
+        deck.push({s : suits[0], r : j+2});
+      }
+      count++;
+    }
 
-    
-
-//You can test the below functions by creating e.g., a `pokerHand` array that 
+//You can test the below functions by creating e.g., a `pokerHand` array that
 //contains five cards from the `deck`.
 
 /* Define a function `containsQueenOfHearts()` that takes in an array of "card"
@@ -72,24 +111,24 @@
    is in that array.
    Hint: use a loop to check each card. */
 
-   
+
 
 /* Define a function `getHighCard()` that takes in an array of "card" objects
   and returns the card object with the highest value. The "high card" is the one
-  with the highest rank. Cards of different suits but the same rank are 
+  with the highest rank. Cards of different suits but the same rank are
   considered to have the same value, and either is a valid result */
 
-  
+
 
 /* Define a function `isFlush()` that takes in an array of "card" objects and
    returns whether or not the cards all have the same _suit_. */
 
-   
 
-/* Extra challenge: define a function `hasPair()` that takes in an array of 
-   "card" objects and returns whether or not there is at least one _pair_ (two 
+
+/* Extra challenge: define a function `hasPair()` that takes in an array of
+   "card" objects and returns whether or not there is at least one _pair_ (two
    cards with the same _rank_) in the array.
-   Double challenge: return the rank of the pair of cards with the highest rank 
+   Double challenge: return the rank of the pair of cards with the highest rank
    (e.g., if the hand contains more than one pair!) */
 
 
@@ -97,13 +136,13 @@
 //Make functions and variables available to tester. DO NOT MODIFY THIS.
 if(typeof module !== 'undefined' && module.exports){
   /* eslint-disable */
-  if(typeof addFour !== 'undefined') 
+  if(typeof addFour !== 'undefined')
     module.exports.addFour = addFour;
-  if(typeof twelveString !== 'undefined') 
+  if(typeof twelveString !== 'undefined')
     module.exports.twelveString = twelveString;
-  if(typeof compoundInterest !== 'undefined') 
+  if(typeof compoundInterest !== 'undefined')
     module.exports.compoundInterest = compoundInterest;
-  if(typeof fizzBuzz !== 'undefined') 
+  if(typeof fizzBuzz !== 'undefined')
     module.exports.fizzBuzz = fizzBuzz;
   if(typeof getLetterFrequencies !== 'undefined')
     module.exports.getLetterFrequencies = getLetterFrequencies;
